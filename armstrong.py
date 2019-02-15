35be4f3edc18e678698baaec24bff5f42f59250b
JijithaRK <47183088+JijithaRK@users.noreply.github.com>
2019-02-15 13:05:29 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,1 +1,13 @@
-gdh
+n11 = int(input())  
+s11 = 0  
+t3 = n11 
+  
+while t3 > 0:  
+   digi = t3 % 10  
+   s11 =s11 + digi ** 3  
+   t3 //= 10  
+  
+if n11 == s11:  
+   print("yes")  
+else:  
+   print("no")
